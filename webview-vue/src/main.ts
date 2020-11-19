d2e36fe14eb87f02b993d0537da7a4984f390f1d
wjkang <ruoxieme@gmail.com>
2020-11-19 23:49:42 +0800

    add ant-design-vue

--- webview-vue/src/main.ts
+++ webview-vue/src/main.ts
@@ -1,9 +1,8 @@
 import { createApp } from 'vue';
-import ElementPlus from 'element-plus';
-import 'element-plus/lib/theme-chalk/index.css';
 import App from './App';
 import router from './router';
 import store from './store';
+import 'ant-design-vue/dist/antd.css';
 import '/@/style/index.scss';
 
-createApp(App).use(ElementPlus).use(router).use(store).mount('#root');
+createApp(App).use(router).use(store).mount('#root');
