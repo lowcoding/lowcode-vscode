5c0797017c32dfaa43bb783aa47062fd8b4c9063
wjkang <ruoxieme@gmail.com>
2020-12-03 00:09:25 +0800

    feat: 单元测试

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -6,10 +6,11 @@ import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+  console.log(12121);
+  vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
-	});
+  test('Sample test', () => {
+    assert.equal(-1, [1, 2, 3].indexOf(5));
+    assert.equal(-1, [1, 2, 3].indexOf(0));
+  });
 });
